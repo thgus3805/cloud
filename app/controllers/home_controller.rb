c7788963a713209fbdc17c6f7f26a84c4938a7a5
So Hyeon Youn <thgus3805@naver.com>
2018-04-05 11:40:26 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def create
+    @post = Post.new
+    @post.db_title = params[:title]
+    @post.db_content = params[:content]
+    @post.save
+    
+    redirect_to '/read'
+    
+  end
+
+  def read
+    @posts = Post.all
+  end
+end
